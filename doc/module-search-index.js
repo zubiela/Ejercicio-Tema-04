c81c462df34a82607f172c00b4113d1f26e9889f
zubiela <pzubiela@gmail.com>
2023-03-15 11:54:20 +0100

    Subido el JAvaDoc

---
+++ doc/module-search-index.js
@@ -0,0 +1,1 @@
+moduleSearchIndex = [{"l":"Tema04"}];updateSearchResults();
--- src/Cuentas/CCuenta.java
+++ src/Cuentas/CCuenta.java
@@ -1,29 +1,67 @@
+/**
+     * @autor Pablo Zubiela
+     * @version 1.0 marzo 2023
+     */
 package Cuentas;
-
+/**
+ * Clase para representar una cuenta bancaria.
+ */
 public class CCuenta {
 
-
+	 /**
+     * Nombre del titular de la cuenta.
+     */
     private String nombre;
+    
+    /**
+     * Número de cuenta.
+     */
     private String cuenta;
+    
+    /**
+     * Saldo de la cuenta.
+     */
     private double saldo;
+    
+    /**
+     * Tipo de interés de la cuenta.
+     */
     private double tipoInterés;
 
+    /**
+     * atrib de la cuenta.
+     */
     public CCuenta()
     {
+    	
     }
-
+    
+    /**
+     * inicializa todos los atributos de la cuenta.
+     * @param nombre El nombre del titular de la cuenta.
+     * @param cuenta El número de cuenta.
+     * @param saldo El saldo actual de la cuenta.
+     * @param tipoInteres El tipo de interés de la cuenta.
+     */
     public CCuenta(String nom, String cue, double sal, double tipo)
     {
         setNombre(nom);
         setCuenta(cue);
         setSaldo(sal);
     }
-
+    /**
+     * Devuelve el saldo actual de la cuenta
+     */
     public double estado()
     {
         return getSaldo();
     }
 
+    /**
+     * Ingresa una cantidad en la cuenta.
+     * @param cantidad La cantidad a ingresar.
+     * @throws Exception Si la cantidad es negativa.
+     */
     public void ingresar(double cantidad) throws Exception
     {
         if (cantidad<0)
@@ -31,6 +69,11 @@ public class CCuenta {
         setSaldo(getSaldo() + cantidad);
     }
 
+    /**
+     * Retira una cantidad de la cuenta.
+     * @param cantidad La cantidad a retirar.
+     * @throws Exception Si la cantidad es negativa o si no hay suficiente saldo en la cuenta.
+     */
     public void retirar(double cantidad) throws Exception
     {
         if (cantidad <= 0)
